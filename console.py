d291b95869c34ec1b38269128ad8f660b0fa2611
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-08-05 02:51:44 +0200

    console.py

---
+++ console.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import cmd
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
